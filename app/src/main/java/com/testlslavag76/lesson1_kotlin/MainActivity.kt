7a76b9e591398e5dc229e05a123e0580d9505869
SlavaGr <Slavag76@yandex.ru>
2021-07-25 20:49:04 +0300

    add count button

--- app/src/main/java/com/testlslavag76/lesson1_kotlin/MainActivity.kt
+++ app/src/main/java/com/testlslavag76/lesson1_kotlin/MainActivity.kt
@@ -1,18 +1,38 @@
 package com.testlslavag76.lesson1_kotlin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity : AppCompatActivity() {
+
+
+    var count: Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initCount()
     }
 
     fun toastMe(view: View) {
         val myToast = Toast.makeText(this, "Hello, Toast!", Toast.LENGTH_SHORT)
         myToast.show()
+    }
+
+    fun initCount() {
+
+        var textView = findViewById<TextView>(R.id.textView)
+        var buttonCount = findViewById<Button>(R.id.countButton)
+
+        buttonCount.setOnClickListener {
+            count++
+            textView.text = count.toString()
+        }
+
     }
 }
